36a83ca175f25f363b969911af30f495f3b6a498
Sergey Popov <serzh.serp123.popov@mail.ru>
2024-11-07 16:55:48 +0700

    Переместил MainActivity в пакет presentation

--- app/src/main/java/ru/sergey/myfilmsapp/presentation/MainActivity.kt
+++ app/src/main/java/ru/sergey/myfilmsapp/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package ru.sergey.myfilmsapp
+package ru.sergey.myfilmsapp.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ru.sergey.myfilmsapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
