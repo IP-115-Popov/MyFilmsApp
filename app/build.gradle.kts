212e2ead0ad5247b11de376ec45308e550a3e8bd
Sergey Popov <serzh.serp123.popov@mail.ru>
2024-11-09 00:59:14 +0700

    Поменял compileSdk и targetSdk на 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "ru.sergey.myfilmsapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ru.sergey.myfilmsapp"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
